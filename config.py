2455806ca81ae9c23a1bfcafbba239fd726da3e8
PavelDemin <papyasishe@yandex.ru>
2019-12-06 16:33:15 +0300

    init

---
+++ config.py
@@ -0,0 +1,3 @@
+url = 'http://admin:admin@172.16.90.67/printer_config_write.html'
+cake2 = 'cake2.txt'
+cake6 = 'cake6.txt'
---
+++ main.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import config
+
+
+
+#chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+chrome_options = Options()
+#chrome_options.binary_location = chrome_bin
+
+chrome_options = Options()
+#chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--no-sandbox")
+CHROMEDRIVER_PATH = 'C:\\chromedriver.exe'
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+
+
+if __name__ == "__main__":
+    driver.get(config.url)
+    
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "PrnConfWriteSelectDisp"))
+        )
+        # driver.find_element_by_id("PrnConfWriteSelectDisp").send_keys("file-path")
+        # driver.find_element_by_id("submit").click()
+    finally:
+        driver.quit()
